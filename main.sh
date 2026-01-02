b3bbdb800749213c386b52edb42dd5ceee698dbe
ADSmith-0 <adam00199382818@gmail.com>
2026-01-02 00:42:08 +0000

    Added code comment for pad function

--- main.sh
+++ main.sh
@@ -21,6 +21,7 @@ max_length=20
 tmux_icon=""
 git_icons=" "
 
+# Taken from here: https://stackoverflow.com/a/72068155/6515818
 pad() {
   [ "$#" -gt 1 ] && [ -n "$2" ] && printf "%$2.${2#-}s" "$1";
 }
