e1437cbcbc16457196e1b86da0419840b91e6342
ADSmith-0 <adam00199382818@gmail.com>
2026-01-05 22:28:09 +0000

    Fixed updating issue?

--- main.sh
+++ main.sh
@@ -39,8 +39,8 @@ tmux set-window-option -g status-left-length 99
 tmux set-window-option -g status-right-length 99
 tmux set-window-option -g status-justify centre
 # Tmux session
-tmux set-window-option -g status-right "#[fg=${session}] #(~/.tmux/plugins/tmux-one-dark-minimalist/plugins/session.sh)  "
+tmux set-window-option -g status-right '#[fg=#{'"$session"'}] #(~/.tmux/plugins/tmux-one-dark-minimalist/plugins/session.sh)  '
 # Git branch
-tmux set-window-option -g status-left "  #[fg=${git}] #(~/.tmux/plugins/tmux-one-dark-minimalist/plugins/git.sh)"
+tmux set-window-option -g status-left '  #[fg=#{'"$git"'}] #(~/.tmux/plugins/tmux-one-dark-minimalist/plugins/git.sh)'
 tmux set-window-option -g window-status-format " #[fg=${flags}]#{?window_flags,#{window_flags},}#[fg=${unselected_fg}]#I:#W "
 tmux set-window-option -g window-status-current-format "#[fg=${selected_bg},bg=default]#[fg=${selected_fg},bg=${selected_bg}]#{?window_flags,#{window_flags},}#I:#W#[fg=${selected_bg},bg=default]"
