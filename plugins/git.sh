88c2264af80be0b67da163f64ff7084b4a35a671
ADSmith-0 <adam00199382818@gmail.com>
2026-01-05 23:17:52 +0000

    Removed functions

--- plugins/git.sh
+++ plugins/git.sh
@@ -3,14 +3,11 @@
 current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$current_dir/../lib/utils.sh"
 
-get_branch() {
-  result=$(git branch --show-current)
-  length=$(echo $result | tr -d '\n' | wc -m)
-  if [ $length -ge $max_length ]; then
-    echo "$git_icon $(echo $result | head -c $(($max_length - 3)))..."
-  else
-    echo "$(pad "$git_icon $result" -$max_length)"
-  fi
-}
-
-get_branch
+path=$(tmux display-message -p '#{pane_current_path}')
+result=$(git -C "$path" rev-parse --abbrev-ref HEAD)
+length=$(echo $result | tr -d '\n' | wc -m)
+if [ $length -ge $max_length ]; then
+  echo "$git_icon $(echo $result | head -c $(($max_length - 3)))..."
+else
+  echo "$(pad "$git_icon $result" -$max_length)"
+fi
--- plugins/session.sh
+++ plugins/session.sh
@@ -3,14 +3,10 @@
 current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$current_dir/../lib/utils.sh"
 
-get_session() {
-  result=$(tmux display-message -p '#S')
-  length=$(echo $result | tr -d '\n' | wc -m)
-  if [ $length -ge $max_length ]; then
-    echo "...$(echo $result | tail -c $(($max_length - 3))) $tmux_icon"
-  else
-    echo "$(pad "$result $tmux_icon" $max_length)"
-  fi
-}
-
-get_session
+result=$(tmux display-message -p '#S')
+length=$(echo $result | tr -d '\n' | wc -m)
+if [ $length -ge $max_length ]; then
+  echo "...$(echo $result | tail -c $(($max_length - 3))) $tmux_icon"
+else
+  echo "$(pad "$result $tmux_icon" $max_length)"
+fi
