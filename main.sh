5c16c51928055f3dfa6f2ea8fae8c37cf034ed39
ADSmith-0 <adam00199382818@gmail.com>
2026-01-06 01:09:50 +0000

    Fixed updating issue

--- main.sh
+++ main.sh
@@ -12,11 +12,6 @@ get_tmux_option() {
     fi
 }
 
-max_length=30
-
-tmux_icon=""
-git_icon=""
-
 black=$(get_tmux_option "@tmux-one-dark-black" '#282c34')
 red=$(get_tmux_option "@tmux-one-dark-red" '#ef596f')
 green=$(get_tmux_option "@tmux-one-dark-green" '#89ca78')
@@ -46,8 +41,8 @@ tmux set-option -g status-left-length 99
 tmux set-option -g status-right-length 99
 tmux set-option -g status-justify centre
 # Tmux session
-tmux set-option -g status-right '#[fg=#{@session}] #(~/.tmux/plugins/tmux-one-dark-minimalist/plugins/session.sh)  '
+tmux set-option -g status-right '#[fg=#{@session}] #{=/-20/...:session_name}   '
 # Git branch
-tmux set-option -g status-left '  #[fg=#{@git}] #(~/.tmux/plugins/tmux-one-dark-minimalist/plugins/git.sh)'
+tmux set-option -g status-left '  #[fg=#{@git}]  #{=/20/...:#(git -C "#{pane_current_path}" rev-parse --abbrev-ref HEAD)}'
 tmux set-window-option -g window-status-format ' #[fg=#{@flags}]#{?window_flags,#{window_flags},}#[fg=#{@unselected_fg}]#I:#W '
 tmux set-window-option -g window-status-current-format '#[fg=#{@selected_bg},bg=default]#[fg=#{@selected_fg},bg=#{@selected_bg}]#{?window_flags,#{window_flags},}#I:#W#[fg=#{@selected_bg},bg=default]'
